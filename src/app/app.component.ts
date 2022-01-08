baf93fd309a82734e9ce23336a86bc30d4bd2a23
MaximB <maximb@operative.com>
2022-01-09 00:07:43 +0200

    user name page is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
+import {AbstractControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'google';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  title: 'google';
+  form;
+  buttonDisabled = true;
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.form = this.formBuilder.group(
+      {
+        userName: ['',
+          [
+          Validators.nullValidator,
+            Validators.pattern('^[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ ]+$'),
+            Validators.required]]
+      }
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.form.get('userName').valueChanges.subscribe(() => {
+      const userNameValue: string = this.form.get('userName').value;
+      this.buttonDisabled = !/^[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ ]+$/.test(userNameValue);
+    });
+  }
+
+
+  constructor(private primengConfig: PrimeNGConfig,
+              private formBuilder: FormBuilder) {}
+
+
+  onClick(): void {
+
+  }
+
+  isError(key: string, error = 'required'): boolean {
+    const control: AbstractControl = this.form.get(key);
+    return control.hasError(error) && control.touched && control.pending;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {RippleModule} from 'primeng/ripple';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SearchContainerComponent } from './search-container/search-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchContainerComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        InputTextModule,
+        CheckboxModule,
+        ButtonModule,
+        RadioButtonModule,
+        RippleModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
